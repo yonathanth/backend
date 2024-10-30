fd0a7d4d489e16a3f4f2bdcce2280382355987c2
yonathanth <yonathantenaye@gmail.com>
2024-10-30 07:54:06 +0300

    initial

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/employees", require("./routes/employeesRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/stock", require("./routes/stockRoutes"));
+app.use("/api/finance", require("./routes/financialRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
